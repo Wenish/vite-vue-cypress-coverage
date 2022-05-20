1e1721502ec7c18b49d97b19db7c5adb5c6b491a
Wenish <Wenish@users.noreply.github.com>
2022-05-20 20:39:26 +0200

    add vite istanbul plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  build: {
+    sourcemap: 'inline'
+  },
+  plugins: [
+    vue(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: ['.js', '.ts', '.vue'],
+      requireEnv: false,
+    }),]
 })
